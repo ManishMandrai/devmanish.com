758ed7ca3aec0febd46f648d33cb62d6bc08da40
ManishMandrai <manishkumarmandrai@gmail.com>
2025-03-04 15:43:47 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
-
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
+});
